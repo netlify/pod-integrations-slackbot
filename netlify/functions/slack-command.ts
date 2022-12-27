844f1620bb656f94c3136fd745ae0482d41a436a
Tara Z. Manicsic <tzmanics@gmail.com>
2022-12-27 15:54:49 -0500

    changes slack commad file

--- netlify/functions/slack-command.ts
+++ netlify/functions/slack-command.ts
@@ -11,43 +11,34 @@ async function getRequestModalBlocks(commandText: string) {
   // load dropdown options from Notion
   const notionData = await notionApi(`/databases/${process.env.NOTION_DB_ID}`);
   const dbData = convertNotionDataToEasyKeys(notionData.properties);
-  const options = dbData.importance.select.options.map((option) => {
-    return {
-      text: {
-        type: 'plain_text',
-        text: option.name,
-      },
-      value: option.name,
-    };
-  });
 
   return [
     {
       type: 'section',
       text: {
         type: 'mrkdwn',
-        text: `Fill out this form to submit a new DXE request! Or, if you prefer, you can <${notionPageUrl}|create your request in Notion>. Requests are reviewed twice weekly. If your request is urgent, notify <@UPD6LDBFG>.`,
+        text: `Fill out this form to submit a new project update! Or, if you prefer, you can <${notionPageUrl}|create your update in Notion>.`,
       },
     },
     {
-      block_id: 'title_block',
+      block_id: 'project_block',
       type: 'input',
       label: {
         type: 'plain_text',
-        text: 'What do you need?',
+        text: 'Wha project?',
       },
       element: {
-        action_id: 'title',
+        action_id: 'project',
         type: 'plain_text_input',
         placeholder: {
           type: 'plain_text',
-          text: 'Example: Run a DX audit on Netlify Graph in prep for Labs launch',
+          text: 'Example: Planetscale to Labs',
         },
         initial_value: commandText,
       },
       hint: {
         type: 'plain_text',
-        text: 'Summarize your request in one sentence.',
+        text: 'Do not mess this up',
       },
     },
     {
@@ -64,109 +55,32 @@ async function getRequestModalBlocks(commandText: string) {
       },
       label: {
         type: 'plain_text',
-        text: 'When do you need this by?',
+        text: 'What day is it?',
         emoji: true,
       },
       hint: {
         type: 'plain_text',
-        text: 'If your request doesn’t have a due date, we’ll treat it as an idea.',
+        text: 'What day is this update for?',
       },
       optional: true,
-    },
-    {
-      block_id: 'importance_block',
-      type: 'input',
-      label: {
-        type: 'plain_text',
-        text: 'What’s the importance of this request?',
-      },
-      element: {
-        action_id: 'importance',
-        type: 'static_select',
-        options,
-        initial_option: options.at(0),
-      },
-      hint: {
-        type: 'plain_text',
-        text: 'Is there any risk if we don’t do this?',
-      },
     },
     {
       block_id: 'description_block',
       type: 'input',
       label: {
         type: 'plain_text',
-        text: 'Additional details',
+        text: 'What is up?',
       },
       element: {
         action_id: 'description',
         type: 'plain_text_input',
         multiline: true,
-        placeholder: {
-          type: 'plain_text',
-          text: 'Example: Links to project plans, background, context, etc.',
-        },
       },
       hint: {
         type: 'plain_text',
-        text: 'Details help us to understand the goal, which helps us prioritize.',
+        text: 'Give us all the deets.',
       },
-      optional: true,
-    },
-  ];
-}
-
-async function getIdeaModalBlocks(commandText: string) {
-  return [
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: 'Got a great idea for DXE? Capture it here! Ideas do not have deadlines or hard dependencies — they’re the “wouldn’t it be cool if...” ideas we don’t want to lose.',
-      },
-    },
-    {
-      block_id: 'title_block',
-      type: 'input',
-      label: {
-        type: 'plain_text',
-        text: 'What do you have in mind?',
-      },
-      element: {
-        action_id: 'title',
-        type: 'plain_text_input',
-        placeholder: {
-          type: 'plain_text',
-          text: 'Example: Create an interactive library of examples for using Edge Functions',
-        },
-        initial_value: commandText,
-      },
-      hint: {
-        type: 'plain_text',
-        text: 'Summarize your idea in one sentence.',
-      },
-    },
-    {
-      block_id: 'description_block',
-      type: 'input',
-      label: {
-        type: 'plain_text',
-        text: 'Additional details',
-      },
-      element: {
-        action_id: 'description',
-        type: 'plain_text_input',
-        multiline: true,
-        placeholder: {
-          type: 'plain_text',
-          text: 'Example: Why is this cool? How does it help? Who is it for?',
-        },
-      },
-      hint: {
-        type: 'plain_text',
-        text: 'Details help us to understand the goal, which helps us prioritize.',
-      },
-      optional: true,
+      optional: false,
     },
   ];
 }
@@ -185,14 +99,9 @@ export const handler: Handler = async (event) => {
   let blocks;
   let callbackId;
   switch (command) {
-    case '/dxreq':
+    case '/integrations-update':
       blocks = await getRequestModalBlocks(text as string);
-      callbackId = 'dx.submit-request';
-      break;
-
-    case '/dxidea':
-      blocks = await getIdeaModalBlocks(text as string);
-      callbackId = 'dx.submit-idea';
+      callbackId = 'integrations.update';
       break;
 
     default:
@@ -208,7 +117,7 @@ export const handler: Handler = async (event) => {
       type: 'modal',
       title: {
         type: 'plain_text',
-        text: 'Submit a DX Eng Request',
+        text: 'Submit an Integrations update.',
       },
       callback_id: callbackId,
       submit: {
